049fc48fb4aa73ad935799bc679c6b4761c1d115
Chris Reed <chris@reedtech.us>
2024-02-14 04:38:34 -0600

    feat: integrate JotForm API with SDK
    Added JotForm API integration in Laravel package. Defined API key configuration in 'config/jotform-sdk.php'. Updated 'JotformSDK' facade return type for clarity. Implemented `api` method in `JotformSDK` to provide access to JotForm API. Refactored 'JotformSDKServiceProvider' to register JotForm API as a singleton. Updated `composer.json` to include necessary packages for the integration.

--- src/JotformSDK.php
+++ src/JotformSDK.php
@@ -2,6 +2,13 @@
 
 namespace ChrisReedIO\JotformSDK;
 
+use Illuminate\Support\Facades\App;
+use JotForm;
+
 class JotformSDK
 {
+    public static function api(): JotForm
+    {
+        return App::make(JotForm::class);
+    }
 }
